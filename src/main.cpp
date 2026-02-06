c7ec43d4dfea1716a0cdb4b0838ed233871cf025
ikathesunbro <ikathesunbro@gmail.com>
2026-02-07 01:18:27 +0300

    fixed shaders :d

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,80 @@ int main()
 {
 	glfwInit();
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(SRC_WIDTH, SRC_HEIGHT, "Hikachi", NULL, NULL);
+
+	if (!window)
+	{
+		std::cout << "Failed to create window :(" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD :(" << std::endl;
+		glfwTerminate();
+		return -1;		
+	}
+
+	Shader shader("shader/default.vert", "shader/default.frag");
+
+	unsigned int VAO, VBO;	
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	
+	glBindVertexArray(0);
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+		glClearColor(0.3f, 0.4f, 0.43f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		shader.use();
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
 
 	glfwTerminate();
 	return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
 }
--- src/main.h
+++ src/main.h
@@ -4,3 +4,23 @@
 
 #include <GLAD/glad.h>
 #include <GLFW/glfw3.h>
+
+#include "shader.h"
+
+// Global Settings
+const unsigned int SRC_WIDTH{ 800 };
+const unsigned int SRC_HEIGHT{ 600 };
+
+// Data
+float vertices[] = {
+     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  
+    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  
+     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   
+};
+
+
+
+// Functions
+void processInput(GLFWwindow* window);
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
